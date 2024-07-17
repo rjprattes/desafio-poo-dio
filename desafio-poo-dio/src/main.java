105ea4c0b0f237a2eb630efa2fefbfd6bbcd3de7
rjprattes <85262485+rjprattes@users.noreply.github.com>
2024-07-17 15:06:44 -0300

    Mentoria e Curso

---
+++ desafio-poo-dio/src/main.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição");
+        curso1.setCargaHoraria(200);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("JS");
+        curso2.setDescricao("Descrição");
+        curso2.setCargaHoraria(300);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Mentoria JS");
+        mentoria2.setDescricao("Descrição mentoria JS");
+        mentoria2.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+        System.out.println(mentoria2);
+    }
+}
